b8051a34b7ef0ea9dad612cc3b8081aa499748a2
Junjie Yang <junjie@jyang.dev>
2024-04-19 11:31:04 -0700

    add switch

--- socket/client/src/App.jsx
+++ socket/client/src/App.jsx
@@ -7,9 +7,18 @@ export const App = () => {
     const [streaming, setStreaming] = useState(false);
     const videoRef = useRef(null);
     const canvasRef = useRef(null);
-    const [timestamps, setTimestamps] = useState([]);
+    const [devices, setDevices] = useState([]);
+    const [curDevice, setCurDevice] = useState();
+    const getUserDevices = async () => {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+        setDevices(videoDevices);
+        setCurDevice(videoDevices[0].deviceId);
+    }
     useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ video: true })
+        navigator.mediaDevices.getUserMedia({
+            video: true
+        })
             .then(stream => {
                 videoRef.current.srcObject = stream;
                 videoRef.current.onloadedmetadata = () => {
@@ -17,13 +26,26 @@ export const App = () => {
                 };
             })
             .catch(console.error);
-
         socket.on('frame_processed', data => {
-            data['client-received'] = new Date().getTime().toString();
-            setTimestamps((prev) => [...prev, data]);
             drawBoundingBoxes(data['frame'], data['cls']);
         });
+        getUserDevices();
     }, []);
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({
+            video: {
+                deviceId: curDevice
+            }
+        })
+            .then(stream => {
+                videoRef.current.srcObject = stream;
+                videoRef.current.onloadedmetadata = () => {
+                    setStreaming(true);
+                };
+            })
+            .catch(console.error);
+    }, [curDevice]);
 
     const drawBoundingBoxes = (boxes, classes) => {
         const context = canvasRef.current.getContext('2d');
@@ -60,36 +82,34 @@ export const App = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             captureFrame();
-        }, 200);
+        }, 100);
         return () => clearInterval(interval);
     }, [streaming]);
 
     return (
         <div className='flex flex-col h-screen w-screen justify-between'>
-            <div className='h-1/4 w-full' />
-            <div className='flex h-1/2 justify-center'>
+            <div className='flex flex-col h-1/4 w-full self-center justify-center'>
+                <span className='self-center text-7xl font-bold'>Demo</span>
+            </div>
+            <div className='flex flex-1 justify-center self-center'>
                 <video ref={videoRef} autoPlay style={{ display: 'none' }}></video>
-                <canvas className='w-1/2 h-full' ref={canvasRef} width={1920} height={1080} />
+                <canvas className='w-full h-full' ref={canvasRef} width={1920} height={1080} />
                 <div className='overflow-auto flex-1'>
-                    {timestamps && timestamps.length > 0 && timestamps.map(timestamp => {
-                        return (
-                            <div className='flex flex-col border-2 border-black my-4'>
-                                <div className='flex justify-around'>
-                                    <span>Client Send: {timestamp['client-start']}</span>
-                                    <span>Server Received: {timestamp['server-received']}</span>
-                                    <span className='text-red-500'>Diff: {timestamp['server-received'] - timestamp['client-start']}</span>
-                                </div>
-                                <div className='flex justify-around'>
-                                    <span>Server Processed: {timestamp['server-processed']}</span>
-                                    <span>Client Received: {timestamp['client-received']}</span>
-                                    <span className='text-red-500'>Diff: {timestamp['client-received'] - timestamp['server-processed']}</span>
-                                </div>
-                            </div>
-                        )
-                    })}
                 </div>
             </div>
-            <div className='h-1/4 w-full' />
+            <div className='h-1/4 w-full flex flex-col justify-center'>
+                <div className='flex justify-center gap-4'>
+                {
+                    devices && devices.map((device) => {
+                        return (
+                            <div key={device.deviceId} onClick={() => setCurDevice(device.deviceId)} className={`${device.deviceId === curDevice ? 'bg-blue-500': 'border border-blue-500'}`}>
+                                <span>{device.label}</span>
+                            </div>
+                        )
+                    })
+                }
+                </div>
+            </div>
         </div>
     );
 }
